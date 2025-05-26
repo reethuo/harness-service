b72cd4533cd89d733d4d7567943ff5375240d76c
reethuo <152209084+reethuo@users.noreply.github.com>
2025-05-26 17:19:21 +0530

    Update service-create.tf

--- service-create.tf
+++ service-create.tf
@@ -5,59 +5,44 @@ terraform {
       version = "0.14.0"
     }
   }
-  # required_version = "1.3.9"
 }
-
-resource "harness_platform_service" "this" {
-  identifier   = "git-trigger-terraform-service"
-  name         = "git-trigger-terraform-service"
-  org_id       = "default"
-  project_id   = "SFTY_Training"
-  description  =  "adding description"
-  yaml         = <<-EOH
-service:
-  name: git-trigger-terraform-service
-  identifier: git-trigger-terraform-service
-  orgIdentifier: default
-  projectIdentifier: SFTY_Training
-  serviceDefinition:
-    spec:
-      manifests:
-        - manifest:
-            identifier: manifest1
-            type: K8sManifest
-            spec:
-              store:
-                type: Github
-                spec:
-                  connectorRef: <+input>
-                  gitFetchType: Branch
-                  paths:
-                    - infra-vm.tf
-                  repoName: tera0
-                  branch: main
-              skipResourceVersioning: false
-      /* artifacts:
-        primary:
-          primaryArtifactRef: <+input>
-          sources:
-            - spec:
-                connectorRef: rajharnessdev
-                imagePath: harnessdev/rajtodoapp
-                tag: "4"
-                digest: ""
-              identifier: todofrompipeline
-              type: DockerRegistry */
-    type: Kubernetes
-  EOH
-}
-
-/* output "out" {
-  value = harness_platform_service.this.id
-} */
 
 provider "harness" {
   endpoint         = "https://app.harness.io/gateway"
   account_id       = "ucHySz2jQKKWQweZdXyCog"
-  platform_api_key = ""
+  platform_api_key = "your_platform_api_key_here"
+}
+
+resource "harness_platform_service" "this" {
+  identifier  = "example-service"
+  name        = "example-service"
+  org_id      = "default"
+  project_id  = "MyProject"
+  description = "Created by Terraform"
+
+  yaml = <<-EOT
+    service:
+      name: example-service
+      identifier: example-service
+      orgIdentifier: default
+      projectIdentifier: MyProject
+      serviceDefinition:
+        type: Kubernetes
+        spec:
+          manifests:
+            - manifest:
+                identifier: manifest1
+                type: K8sManifest
+                spec:
+                  store:
+                    type: Github
+                    spec:
+                      connectorRef: account.GithubConnector
+                      gitFetchType: Branch
+                      branch: main
+                      repoName: example-repo
+                      paths:
+                        - k8s/manifest.yaml
+                  skipResourceVersioning: false
+  EOT
 }
